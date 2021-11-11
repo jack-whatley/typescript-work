2ae428575636d717ef408a07c610f7559287c004
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-11 09:36:49 +0000

    object experimentation

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-var message = 'Hello there...';
-console.log(message);
+var jack = {
+    name: 'Jack',
+    age: 16,
+    address: ['Im', 'Not', 'Sharing']
+};
+console.log(jack);
--- index.ts
+++ index.ts
@@ -1,2 +1,18 @@
-let message: string = 'Hello there...';
-console.log(message);
+interface Person {
+
+    name: string;
+    age: number;
+    address: string[];
+
+}
+
+const jack: Person = {
+
+    name: 'Jack',
+    age: 16,
+    address: ['Im', 'Not', 'Sharing']
+
+}
+
+console.log(jack);
+ 
